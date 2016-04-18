6aff012f87c421b34b5afe6b74568f680dadf24c
redpanda321 <redpanda321@hotmail.com>
2016-04-18 17:43:28 -0600

    v0.0.7

--- doc/SQLQuery1.sql
+++ doc/SQLQuery1.sql
@@ -1,5 +1,9 @@
 ﻿
 
+select convert(nvarchar(30),getdate()-1,111);
 
-select count(*) from resulthistories;
 
+select  * from resulthistories t1 
+inner join resulthistories t2  on t1.resultId = t2.resultId and  t1.Price < t2.Price  where t1.resultdatetime > convert(nvarchar(30),getdate(),111) and 
+
+t2.resultdatetime < convert(nvarchar(30),getdate(),111) ; 
