0d423573df164cae25856d39a48299b3f95162c1
redpanda321 <redpanda321@hotmail.com>
2016-04-21 17:25:24 -0600

    v0.0.7

--- doc/SQLQuery1.sql
+++ doc/SQLQuery1.sql
@@ -1,9 +1,31 @@
 ﻿
 
-select convert(nvarchar(30),getdate()-1,111);
+select count(*) from results;
+select count(*) from resulthistories;
 
 
-select  * from resulthistories t1 
-inner join resulthistories t2  on t1.resultId = t2.resultId and  t1.Price < t2.Price  where t1.resultdatetime > convert(nvarchar(30),getdate(),111) and 
+/*select  t3.id ,t3.PublicRemaRrks, t3.PostalCode , t3.RelativeDetailsURL from  results  t3 where t3.RelativeDetailsURL like '%calgary%';  */
 
-t2.resultdatetime < convert(nvarchar(30),getdate(),111) ; 
+
+select  t1.id, t1.ResultId,t1.resultdatetime, t1.Price,t2.resultdatetime, t2.Price, t3.PostalCode , t3.RelativeDetailsURL from resulthistories t1 
+inner join resulthistories t2  on t1.resultId = t2.resultId and  t1.Price < t2.Price  join results t3 on t1.resultid = t3.id where t1.resultdatetime > convert(nvarchar(30),getdate(),111) and 
+
+t2.resultdatetime > convert(nvarchar(30),getdate()-1,111) and t2.resultdatetime < convert(nvarchar(30),getdate(),111) and  t3.RelativeDetailsURL like '%calgary%';
+
+
+
+/*
+select   t1.id, t1.ResultId,t1.resultdatetime, t1.Price,t2.resultdatetime, t2.Price,  t3.PostalCode , t3.RelativeDetailsURL  from resulthistories t1 
+inner join resulthistories t2  on t1.resultId = t2.resultId and  t1.Price < t2.Price  join results t3 on t1.resultid = t3.id where t1.resultdatetime > convert(nvarchar(30),getdate(),111) and 
+
+t2.resultdatetime > convert(nvarchar(30),getdate()-1,111) and t2.resultdatetime < convert(nvarchar(30),getdate(),111) and   t3.RelativeDetailsURL like '%albert%';
+*/
+
+/*
+
+select  t1.id, t1.ResultId,t1.resultdatetime, t1.Price,t2.resultdatetime, t2.Price, t3.PostalCode , t3.RelativeDetailsURL from resulthistories t1 
+inner join resulthistories t2  on t1.resultId = t2.resultId and  t1.Price < t2.Price  join results t3 on t1.resultid = t3.id where t1.resultdatetime > convert(nvarchar(30),getdate(),111) and 
+
+t2.resultdatetime > convert(nvarchar(30),getdate()-1,111) and t2.resultdatetime < convert(nvarchar(30),getdate(),111) ;
+
+*/
