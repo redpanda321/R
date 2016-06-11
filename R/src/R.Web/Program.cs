f476e0033f199241ee6808dcaa1c0eba6cbd7bba
tony <12070132@qq.com>
2016-06-11 15:35:03 -0600

    v0.1.0_R.Web_WebSocket_2016-6-11

--- R/src/R.Web/Program.cs
+++ R/src/R.Web/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using WebSocketSharp.Server;
+using R.Web.Services;
 
 namespace R.Web
 {
@@ -11,6 +13,21 @@ namespace R.Web
     {
         public static void Main(string[] args)
         {
+
+
+            //WebSocket Server
+            var wssv = new WebSocketServer(5115);
+            wssv.AddWebSocketService<ResultBehavior>("/Result");
+            wssv.Start();
+
+            if (wssv.IsListening)
+            {
+                Console.WriteLine("Listening on port {0}, and providing WebSocket services:", wssv.Port);
+                foreach (var path in wssv.WebSocketServices.Paths)
+                    Console.WriteLine("- {0}", path);
+            }
+
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -19,6 +36,9 @@ namespace R.Web
                 .Build();
 
             host.Run();
+
+           
+
         }
     }
 }
