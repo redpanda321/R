4b45e35573201859a1b69752aff1537e7d7a2643
tony <12070132@qq.com>
2016-07-26 23:44:29 -0600

    v0.1.0_R_EF6

---
+++ R/src/R/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using R.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace R.Migrations
+{
+    
+        public class Configuration : DbMigrationsConfiguration<ApplicationDbContext> 
+	{  	}
+
+
+    
+}
